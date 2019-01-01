7ab97adecd653ad2a02fb3028a2ff42a561d3a4e
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2019-01-01 13:10:48 +0100

    Replaced ToolTipConverter

---
+++ NetworkVideoPlayerFrontend/PositionToolTipConverter.cs
@@ -0,0 +1,33 @@
+﻿using StdOttStandard;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace NetworkVideoPlayerFrontend
+{
+    class PositionToolTipConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return ConvertToString(TimeSpan.FromSeconds((double)value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ConvertToString(TimeSpan span, bool includeMillis = false)
+        {
+            string text = string.Empty;
+            int hours = (int)Math.Floor(span.TotalHours);
+
+            if (hours >= 1) text += string.Format("{0,2}:", hours);
+
+            text += string.Format("{0,2}:{1,2}", span.Minutes, span.Seconds);
+
+            if (includeMillis) text += string.Format(":{0,2}", span.Milliseconds);
+
+            return text.Replace(' ', '0');
+        }
+    }
+}
